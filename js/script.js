1292975c510180abcfce7ba72eea678b1c8b297a
Joey Pellegrino <joepell19@gmail.com>
2017-03-28 22:57:00 -0700

    added setInterval

--- js/script.js
+++ js/script.js
@@ -1,11 +1,16 @@
 var words = ['Web Developer', 'UX Designer', 'Sports Enthusiast', 'Gamer', 'Dog Lover'];
 
-function randomWord() {
-    var rand = words[Math.floor(Math.random() * words.length)];
+function initializeWord(id) {
+    var wordChanger = document.getElementById(id);
     
-    changeTheWord = document.getElementById('changer');
-    changeTheWord.innerHTML = rand;
+    function chooseWord() {
+        var randWord = words[Math.floor(Math.random() * words.length)];
+        wordChanger.innerHTML = randWord;
+        
+    }
     
+    chooseWord(); //runs first to avoid delay
+    var wordInterval = setInterval(chooseWord, 5000);
 }
 
-randomWord();
+initializeWord('changer')
